b38a5d01b644d741cf4898cba902014aa9d6f38d
ChavesArt <arthur.2022311833@aluno.iffar.edu.br>
2024-06-20 17:14:02 -0300

    tipos de criptografia

---
+++ cripto.php
@@ -0,0 +1,31 @@
+<?php
+
+
+$senha = "dallanora";
+echo "Minha senha:$senha <br> <hr>";
+
+echo"Não recomendados <br>";
+//MD5
+$hash = md5($senha);
+echo "<b>MD5:</b>". $hash;
+
+//SHA1
+$hash = sha1($senha);
+
+echo "<br><b>SHA1:</b> ". $hash;
+echo"<hr>";
+
+echo "Recomendados <br>";
+
+//Crypt
+
+$hash = crypt($senha,'iffar');
+
+echo "<b>Crypt:</b> ". $hash;
+
+//password_hash
+$hash = password_hash($senha,PASSWORD_DEFAULT);
+echo "<br><b>Password_hash:</b> ". $hash;
+echo "<hr>";
+
+?>
